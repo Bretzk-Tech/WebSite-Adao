48d726edc043d90e5fb5c1265e3a3a208a220fa9
lucas-bretzke <lucasbretzke@gmail.com>
2025-04-25 17:13:46 -0300

    refactor: update path imports to use node module resolution in vite.config.ts fix: add missing types for Node.js in tsconfig.node.json

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,11 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+// Get the directory name using import.meta.url
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
