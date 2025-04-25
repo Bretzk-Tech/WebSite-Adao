22bb0821f9e3e26e9cddd4126b69f01f94ed6b8b
lucas-bretzke <lucasbretzke@gmail.com>
2025-04-25 17:07:57 -0300

    ...

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
+/// <reference types="node" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { fileURLToPath } from 'url'
 import path from 'path'
-
-// Get the directory name using ESM compatible approach
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
